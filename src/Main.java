eafcdb41beb17879bcac1aebdbb8a5bcf1f50f13
Goncharov_Mikhail <mishgun-nzk@mail.ru>
2022-11-30 17:03:41 +0700

    added hw2

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        int dogs = 8;
+        System.out.println("Значение переменной dogs с типом int равно " + dogs + ".");
+        byte cats = 5;
+        System.out.println("Значение переменной cats с типом bytes равно " + cats + ".");
+        short pinapples = 2;
+        System.out.println("Значение переменной pinapples с типом short равно " + pinapples + ".");
+        long stars = 50_000_000_000_000L;
+        System.out.println("Значение переменной stars с типом long равно " + stars + ".");
+        float pi = 3.1415F;
+        System.out.println("Значение переменной pi с типом float равно " + pi + ".");
+        double number = 5.5;
+        System.out.println("Значение переменной number с типом double равно " + number + ".");
+
+        // Задача 2
+        System.out.println("Задача 2");
+
+
+        // Задача 3
+        System.out.println("Задача 3");
+
+
+        // Задача 4
+        System.out.println("Задача 4");
+
+
+        // Задача 5
+        System.out.println("Задача 5");
+
+
+        // Задача 6
+        System.out.println("Задача 6");
+
+
+        // Задача 7
+        System.out.println("Задача 7");
+
+
+        // Задача 8
+        System.out.println("Задача 8");
+
+
+    }
+}
